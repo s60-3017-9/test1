434482d2a0e415db43681100151b72dc37257c94
aofapp <s6001012630179@email.kmutnb.ac.th>
2019-02-07 14:51:59 +0700

    commitV.3

--- myweb/multi/views.py
+++ myweb/multi/views.py
@@ -5,8 +5,8 @@ def show(request,number):
     # s=[]
     # for i in range(1,12):
     #     s.append(number*i)
-    n = str(number)*3
-    context={'number':n}
+    # n = str(number)*3
+    context={'number':number}
 
 
     return render(request,'multi/show.html',context)
